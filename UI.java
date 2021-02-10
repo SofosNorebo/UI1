6b8f9bf335e1ec4ecf1906d6f052bb99f4043506
SofosNorebo <migueloberono@gmail.com>
2021-02-10 14:41:58 -0600

    Ahora puedo contar clicks

--- UI.java
+++ UI.java
@@ -1,20 +1,35 @@
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.BorderFactory;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+
+public class UI implements ActionListener {
+
+    private int count = 0;
+    private JLabel label;
+    private JFrame frame;
+    private JPanel panel;
 
 
-public class UI{
+    public UI() {
+        frame = new JFrame();
 
-    public UI()
-    {
-        JFrame frame = new JFrame();
+        JButton button = new JButton("E we, picame");
+        button.addActionListener(this);
 
-        JPanel panel = new JPanel();
-        panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
+        label = new JLabel("Clicks");
+
+        panel = new JPanel();
+        panel.setBorder(BorderFactory.createEmptyBorder(50, 50, 10, 50));
         panel.setLayout(new GridLayout(0, 1));
+        panel.add(button);
+        panel.add(label);
 
         frame.add(panel, BorderLayout.CENTER);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -24,8 +39,13 @@ public class UI{
 
     }
 
-public static void main(String[] args) 
-    {
+    public static void main(String[] args) {
         new UI();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        count++;
+        label.setText("Número de clicks " + count);
     }
 }
