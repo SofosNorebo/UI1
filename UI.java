9ded2dd709dfd3bed63ef80416b2d83026928466
SofosNorebo <migueloberono@gmail.com>
2021-02-10 14:17:03 -0600

    Empezando

--- UI.java
+++ UI.java
@@ -1,7 +1,22 @@
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.BorderFactory;
+import javax.swing.GridLayout;
+
 public class UI{
+
+    public UI()
+    {
+        JFrame frame = new JFrame();
+
+        JPanel panel = new JPanel();
+        panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
+        panel.setLayout(new GridLayout());
+
+    }
 
 public static void main(String[] args) 
     {
-        System.out.println("Hello github");
+        new UI();
     }
 }
